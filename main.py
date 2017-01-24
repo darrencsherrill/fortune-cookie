c2b186d10361589ac1dd81b6618a86aae6a131c5
Darren Sherrill <darrencsherrill@gmail.com>
2017-01-23 22:58:06 -0600

    forutne changes

--- main.py
+++ main.py
@@ -15,10 +15,22 @@
 # limitations under the License.
 #
 import webapp2
+import random
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        header = "<h1>Fortune Cookies</h1>"
+        fortunes =["you win get freat fortune","do not seek out what will come to you","you always pass the dutchy to the left hand side","1 milloin dollars is in you future","you will live to 100"]
+        my_fortune = self.random.choice(fortunes)
+        lucky_number = random.randint(1, 999)
+        fortune_sentence = "Fortune: " + my_fortune
+        number_sentence = "Your lucky number is: " + str(lucky_number)
+        number_paragrah = "<p>" + number_sentence + "</p>"
+        self.response.write(header + number_paragrah + number_sentence)
+        fortune = []
+        fortune += ["]
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
